b8d9ff8a2f5b64d1723ca1aee5f5a04676019a3c
boon <bsl3@illinois.edu>
2022-11-20 15:54:44 -0600

    flight

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include "parser.h"
 #include <iostream>
 
-#include <windows.h>
+
 #include <fstream>
 #include <vector>
 #include <map>
 #include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
-
-
-
 int isNumber(string s)
 {
     for (int i = 0; i < s.length(); i++){
@@ -63,18 +60,17 @@ long double distance(long double lat1, long double long1,
     return ans;
 }
 
-map <int, vector<pair<string,pair<double,double>>>>mp;
+
+map <int, vector<pair<int, long double> > > mp;
+vector<std::pair<double, double>> airports;
 int main() {
     //map <string, vector<pair<string, double> > > mp;
   // map <int, vector<pair<int, double> > > mp;
-  SetConsoleOutputCP(CP_UTF8);
-
     ifstream input_file;
     input_file.open("airports.dat");
     if (input_file.good()) {
         string input;
         char cur;
-        string stringz;
         bool isQuote = false;
         vector<string> outputStrings;
         string builtStr = "";
@@ -84,49 +80,22 @@ int main() {
 
       // for(int i = 0; i<20; i++){
       //   input_file >> input;
-      int counter=0;
-
-      while(getline(input_file,input)){
-        counter++;
-        // stringz='αβγδεζηθιξλμνξοπρστυφχψω';
-        //cout<<counter<<endl;
-        //cout<<"ð"<<endl;
-        //stringz="ð";
-        //cout<<"ð"<<endl;
-        // cout<<stringz<<endl;
+      while(std::getline(input_file, input)){
+        bool isNumber=true;
         for(size_t i = 0; i < input.size(); i++){
-          cur = input[i];
+            cur = input[i];
 
-
-          if(!isQuote){
             if(cur == ','){
-              //if(builtStr != ""){
+              if(isNumber && builtStr != ""){
                 outputStrings.push_back(builtStr);
-              //}
+              }
+                isNumber=true;
               builtStr = string();
             }
-            else if(cur == '"'){
-              isQuote = true;
-
-            }
-            else if(cur != ','){
+            else {
+                if(!isdigit(cur) && cur!='-' && cur!='.') isNumber=false;
               builtStr += cur;
             }
-          }
-          else{
-            if(cur == '"'&& i +1 <input.size() ){
-              if(input[i+1]=='"'){
-                builtStr += '"';
-                ++i;
-              }
-              else{
-                isQuote = false;
-              }
-            }
-            else{
-              builtStr += cur;
-            }
-          } 
         }
       //testing 
       // for(size_t i = 0; i < outputStrings.size(); i++){
@@ -135,41 +104,35 @@ int main() {
         //testing
       // cout << "NEXT_LINE" << endl;
       if(!outputStrings.empty()){
-        string index = outputStrings[0];
-        string airport= outputStrings[1];
-        string latitude = outputStrings[6];
-        string longitude=outputStrings[7];
-        int index_id = isNumber(index);
-        double latitude_id = stod(latitude);
-        double longitude_id = stod(longitude);
-
-         // mp[depart].push_back({destination, 0});
-        if(index_id != -1){
-    //mp[index_id].push_back({airport,latitude_id,longlitude_id});
-    mp[index_id].push_back(pair(airport, pair(latitude_id, longitude_id)));
+        string id = outputStrings[0];
+        string lat = outputStrings[1];
+        string lon = outputStrings[2];
+        
+        cout << id << " " << lat << " " << lon <<endl;
+        int id_id = std::stoi(id);
+        double lat_id = std::stod(lat);
+        double lon_id = std::stod(lon);
+        airports.push_back(pair<double, double>(lat_id, lon_id));
         }
         
-      }
       
       outputStrings.clear();
       builtStr = "";
-      isQuote = false;
-    } 
 
       //testing
     //string ss = "2965";
     // for(auto p:mp[ss]){
     //   cout<<p.first << "," << p.second << endl;
     // }
-    
-     for(auto p:mp[12]){
-       cout<<p.first << "," << p.second.first <<","<<p.second.second<< endl;
-     }   
           
-        
+      }
     } 
     else {
         cout << "error filename" << endl;
+    }
+
+    for (int i=0;i<100;i++) {
+        //cout << airports[i].first << " " << airports[i].second << endl;
     }
     input_file.close();
     return 0;
