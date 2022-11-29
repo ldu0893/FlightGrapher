a7d16a0cc3a7f4083da57d7654904c00582b0052
boon <bsl3@illinois.edu>
2022-11-29 11:38:40 -0600

    pagerank algorithm

---
+++ pagerank.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+using namespace std;
+
+
+//pagerank class
+class PageRank
+{
+  public:
+    //adjust adj matrix
+    void makeA(int num, double damping);
+
+    //print adjmatrix
+    void print_adjmatrix();
+
+    //the adj matrix variable, for pagerank
+    vector<vector<double>> A; 
+
+    //the namelist of the airport. contains the airport id
+    vector<int> name_list; 
+
+    //dimension of the adj matrix, namelist, initial vector and result vector
+    int num;
+};
